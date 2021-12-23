e578fb46b049ba8e39e77a2de01ab8388d9abc9c
John Elesho <oluwadamilareelesho@gmail.com>
2021-12-23 12:23:55 +0100

    all commit

---
+++ utils/styles.js
@@ -0,0 +1,113 @@
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  navbar: {
+    position: 'fixed',
+    top: '0',
+    backgroundColor: '#fff',
+    borderBottomWidth: '7',
+    borderBottomStyle: 'solid',
+    borderBottomColor: '#e72a26',
+    '& a': {
+      color: '#203040',
+      marginLeft: 10,
+    },
+  },
+  brand: {
+    fontWeight: 'bold',
+    fontSize: '1.5rem',
+  },
+  grow: {
+    flexGrow: 1,
+  },
+
+  main: {
+    minHeight: '80vh',
+    marginTop: 80,
+  },
+  footer: {
+    marginTop: 10,
+    textAlign: 'center',
+  },
+  section: {
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  form: {
+    width: '100%',
+    maxWidth: 800,
+    margin: '0 auto',
+  },
+  navbarButton: {
+    color: '#203040',
+    textTransform: 'initial',
+  },
+  transparentBackgroud: {
+    backgroundColor: 'transparent',
+  },
+  error: {
+    color: '#f04040',
+  },
+  fullWidth: {
+    width: '100%',
+  },
+  reviewForm: {
+    maxWidth: 800,
+    width: '100%',
+  },
+  reviewItem: {
+    marginRight: '1rem',
+    borderRight: '1px #808080 solid',
+    paddingRight: '1rem',
+  },
+  toolbar: {
+    justifyContent: 'space-between',
+  },
+  menuButton: { padding: 0 },
+  mt1: { marginTop: '1rem' },
+  // search
+  searchSection: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+    },
+  },
+  searchForm: {
+    border: '1px solid #000',
+    backgroundColor: '#ffffff',
+    borderRadius: 5,
+  },
+  searchInput: {
+    paddingLeft: 5,
+    color: '#000000',
+    '& ::placeholder': {
+      color: '#606060',
+    },
+  },
+  iconButton: {
+    backgroundColor: '#e72a26',
+    padding: 5,
+    borderRadius: '0 5px 5px 0',
+    '& span': {
+      color: '#fff',
+    },
+  },
+  sort: {
+    marginRight: 5,
+  },
+
+  fullContainer: { height: '100vh' },
+  mapInputBox: {
+    position: 'absolute',
+    display: 'flex',
+    left: 0,
+    right: 0,
+    margin: '10px auto',
+    width: 300,
+    height: 40,
+    '& input': {
+      width: 250,
+    },
+  },
+}));
+export default useStyles;
